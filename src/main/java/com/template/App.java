8af161ba4ecba263e25510f89886a095a77af161
luciabetelu <lubetelu@gmail.com>
2024-08-27 23:29:48 -0300

    terminado

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,67 @@
 package com.template;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+
+import static com.template.App.deposit;
 
 public class App {
 
-
-    public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+    public static Map<String, Integer> createAccount(String account){
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put(account, 0);
+        return accounts;
     }
 
+    public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
+        Map<String, Integer> updatedAccounts = new HashMap<>();
+        for (Map.Entry<String, Integer> entry: accounts.entrySet()) {
+            if ((amount >= 0) & (entry.getKey().equals(account))) {
+                updatedAccounts.put(account, accounts.get(account) + amount);
+            }
+            else {
+                updatedAccounts.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return updatedAccounts;
+    }
+    public static Map<String, Integer> withdrawal(Map<String, Integer> accounts, String account, int amount){
+        Map<String, Integer> updatedAccounts = new HashMap<>();
+        for (Map.Entry<String, Integer> entry: accounts.entrySet()) {
+            if (amount >= 0 && entry.getKey().equals(account) && entry.getValue() > 0) {
+                    updatedAccounts.put(account, accounts.get(account) - amount);
+            }
+            else {
+                updatedAccounts.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return updatedAccounts;
+    }
+    public static Map<String, Integer> transfer(Map<String, Integer> accounts, String account1, String account2, int amount){
+        Map<String, Integer> updatedAccounts = new HashMap<>();
+        if (accounts.containsKey(account1) & accounts.containsKey(account2)) {
+            for (Map.Entry<String, Integer> entry : accounts.entrySet()) {
+                if (!(account1.equals(account2)) && accounts.get(account1) > 0 && amount > 0) {
+                    if ((entry.getKey().equals(account1))) {
+                        updatedAccounts.put(account1, accounts.get(account1) - amount);
+                    } else if (entry.getKey().equals(account2)) {
+                        updatedAccounts.put(account2, accounts.get(account2) + amount);
+                    } else {
+                        updatedAccounts.put(entry.getKey(), entry.getValue());
+                    }
+                } else {
+                    updatedAccounts.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        else{
+            updatedAccounts.putAll(accounts);
+        }
+       return updatedAccounts;
+
+    }
 
 }
+
+
--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -2,17 +2,92 @@ package com.template;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import static com.template.App.deposit;
+import static com.template.App.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
 
     @Test
+    void createAccount(){
+        Map<String, Integer> accounts = App.createAccount("mati");
+        assertEquals(0, accounts.get("mati"));
+    }
+    @Test
     void testDeposit() {
         Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
         Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
-        assertEquals(true, true);
+        assertEquals(15, updatedAccounts.get("mati"));
+    }
+    @Test
+    void negativeDeposit(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "mati", -5);
+        assertEquals(10, updatedAccounts.get("mati"));
+
+    }
+    @Test
+    void unknownDeposit(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "nico", 5);
+        assertEquals(accounts, updatedAccounts);
+
+    }
+    @Test
+    void testWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdrawal(accounts, "mati", 5);
+        assertEquals(5, updatedAccounts.get("mati"));
+    }
+    @Test
+    void negativeWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdrawal(accounts, "mati", 5);
+        assertEquals(5, updatedAccounts.get("mati"));
+    }
+    @Test
+    void emptyWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 0, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdrawal(accounts, "mati", 5);
+        assertEquals(0, updatedAccounts.get("mati"));
+    }
+    @Test
+    void unknownWithdrawal(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdrawal(accounts, "nico", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void testTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "jaun", 5);
+        assertEquals(5, updatedAccounts.get("mati"));
+        assertEquals(25, updatedAccounts.get("jaun"));
+    }
+    @Test
+    void emptyTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 0, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "jaun", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void unknownTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "nico", "jaun", 5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void negativeTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "jaun", -5);
+        assertEquals(accounts, updatedAccounts);
+    }
+    @Test
+    void autoTransfer(){
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "mati", 5);
+        assertEquals(accounts, updatedAccounts);
     }
 }
